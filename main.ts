2ac193325fa24df99d7f5937216fe7774eed6582
SpicyDogWings <spaceprotodev@proton.me>
2025-05-28 16:23:14 -0600

    feat(ui): nueva interfaz de usuario de bienvenida

--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
 import { keypress } from "@cliffy/keypress";
 import { colors } from "@cliffy/colors";
+import { printHomepage, printMenu } from "./src/utils/userUI.ts";
 
-for await (const event of keypress()) { // inicia el programa
+printHomepage();
+printMenu();
+for await (const event of keypress()) {             // inicia el programa
   if (event.key && ["1"].includes(event.key)){
     console.log(colors.blue("Hola"))
   } else if (event.key === "q") {
