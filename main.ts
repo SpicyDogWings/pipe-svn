6cd8a21263091196475148bcfe6a732c10f56e40
SpicyDogWings <spaceprotodev@proton.me>
2025-05-28 16:22:15 -0600

    feat(core): logo ascii añadido

--- main.ts
+++ main.ts
@@ -4,5 +4,7 @@ import { colors } from "@cliffy/colors";
 for await (const event of keypress()) { // inicia el programa
   if (event.key && ["1"].includes(event.key)){
     console.log(colors.blue("Hola"))
+  } else if (event.key === "q") {
+    Deno.exit()
   }
 }
