729ee466103d67c422a3f737c31ac36e4263fe05
SpicyDogWings <spaceprotodev@proton.me>
2025-05-28 23:02:43 -0600

    feat(views): crear la view de status view

---
+++ src/views/svnStatus.ts
@@ -0,0 +1,49 @@
+import { colors } from "@cliffy/colors";
+import { keypress } from "@cliffy/keypress";
+import { getSvnStatus } from '../utils/svn/svnStatusParser.ts';
+import { printMenuSvnStatus } from "../utils/userUI.ts";
+
+interface StatusLine {
+  path: string;
+  status: string;
+}
+
+const statusView = async () => {
+  console.clear()
+  printMenuSvnStatus()
+  const statusLines = await getSvnStatus();
+  const statusLinesEnchanced: StatusLine[] = []
+  if (statusLines.length > 0) {
+    statusLines.forEach((line) => {
+      const lineEnchanced = line.split(' ').map(item => item.trim()).filter(item => item != '')
+      statusLinesEnchanced.push({ path: lineEnchanced[1], status: lineEnchanced[0] })
+    });
+  }
+  statusLinesEnchanced.forEach((line) => {
+    let statusColor
+    if (line.status === "M") {
+      statusColor = colors.green(line.status)
+    } else if (line.status === "?") {
+      statusColor = colors.white(line.status)
+    }
+    console.log(statusColor, '  ', line.path)
+  })
+  for await (const event of keypress()) {
+    try {
+      if (event.key && ["m"].includes(event.key)) {
+        statusView()
+      } else if (event.key === "q") {
+        Deno.exit();
+      } else {
+        printHomepage();
+        printMenu();
+        console.log()
+        console.log(colors.red("? para acceder al menu"))
+      }
+    } catch (error) {
+      console.error("Algo salió mal");
+    }
+  }
+};
+
+export { statusView };
